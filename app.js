e662322251ff87192a0012f6a41515f648fdc5ac
Sujin Lee <sujinlee@Sujins-MBP-2.fc.ca>
2019-09-26 13:41:02 -0400

    adding

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1'; // this is localhost
-const port = 3000; // this is a node convention
+const port = process.env.PORT || 3000; // this is a node convention
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
